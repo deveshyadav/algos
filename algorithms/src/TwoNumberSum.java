aed990f28affac64fcb9c790e5223f678769557e
Devesh Yadav <deveshyadav19@gmail.com>
2024-10-16 19:05:24 +0530

    Create TwoNumberSum.java

---
+++ algorithms/src/TwoNumberSum.java
@@ -0,0 +1,25 @@
+package net.javaguides.orderservice.config;
+
+import java.util.*;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6 ,8, 14, 0));
+        int target = 14;
+        Collections.sort(numbers);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<numbers.size(); i++)
+        {
+            int n = numbers.get(i);
+            //System.out.println(map);
+            if(map.containsKey(target-n))
+                System.out.println("Pair:-" + n + "," + (target-n));
+            else map.put(n, i);
+        }
+
+    }
+
+
+}
