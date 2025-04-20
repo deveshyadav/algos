81c0e73ff9e24be47d6e5624ba0a2b67e6177294
deveshyadav <deveshyadav19@gmail.com>
2025-04-20 10:52:05 +0530

    Longest consecutive numbers

---
+++ algorithms/src/leetcode/LongestConsecutiveNumbers.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.*;
+
+public class LongestConsecutiveNumbers {
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8,4,3,2,1,7};
+        int max = longestConsecutive(arr);
+        System.out.println(max);
+    }
+}
+
