3ce1a146c4e42eb02a6b18fda6ef0a194cbaffb0
deveshyadav <deveshyadav19@gmail.com>
2025-06-06 21:52:21 +0530

    Practice problems with chatgpt

--- algorithms/src/prepareWithGpt/D6ConsecutiveLongestSeq.java
+++ algorithms/src/prepareWithGpt/D6ConsecutiveLongestSeq.java
@@ -3,8 +3,9 @@ package prepareWithGpt;
 import java.util.*;
 
 public class D6ConsecutiveLongestSeq {
+
     public static void main(String[] args) {
-        List<Integer> list= List.of(1,2,3,4,5,6,7,4,5,6,9,10,11,12,13,14,15,16,17);
+        List<Integer> list = List.of(1,2,3,4,5,6,7,4,5,6,9,10,11,12,13,14,15,16,17);
         int res = consecutiveLongestSeq(list);
         System.out.println(res);
     }
